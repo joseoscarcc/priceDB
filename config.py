640a89ba7e04947e7a23800ab44a322757589cd9
joseoscarcc <94883708+joseoscarcc@users.noreply.github.com>
2023-03-24 17:32:43 -0600

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-# .gitignore should include reference to config.py
-api_key = "YOUR_KEY"
-api_secret = "YOUR_SECRET"
-access_token = "YOUR_ACCESS_TOKEN"
-token_secret = "YOUR_TOKEN_SECRET"
-urlDB ='postgresql://jojumabi:V8qzRc9xzaJ1JhcudwHpXC2gE072vgsE@dpg-cgesosm4dadd1mg46f20-a.oregon-postgres.render.com/pricing_9fln'
-urlCRE = 'https://publicacionexterna.azurewebsites.net/publicaciones/prices'
